39e7dda046dc3a69bd2b9a71dc3e05d778d305a6
Elliot Cunningham <elliot.cunningham@jdc.fr>
2018-10-26 16:08:25 +0200

    create weatherCity Xcode Project with CoreData UnitTest UITest

---
+++ weatherCity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherCity
+//
+//  Created by Elliot Cunningham on 26/10/2018.
+//  Copyright © 2018 Elliot Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
